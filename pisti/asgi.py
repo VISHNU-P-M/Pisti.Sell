a6ef0f438531e39bea696d5d07b23da603aad6fa
Vishnu PM <vishnuputhanalakkal@gmail.com>
2021-05-06 10:43:31 +0530

    add channel layer

--- pisti/asgi.py
+++ pisti/asgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.layers import get_channel_layer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pisti.settings')
 
 application = get_asgi_application()
+channel_layer = get_channel_layer()
